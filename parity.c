80e2952208dc5747a577fd89b9abe8cfc2b02b1f
Alex Aguilar <alexaguilar2@my.unt.edu>
2024-09-11 17:19:50 -0500

    file creation and setup; updated parity.c for cleaner readability

--- parity.c
+++ parity.c
@@ -1,13 +1,21 @@
+/* 
+    Alexis Aguilar (aa1732)
+    parity.c
+
+    parity.c instructions:
+    This code will contain a single function (and the include directive to your header
+    file) to perform the parity computation on the input integer passed to this function.
+    The output of the function is 0 if the input has even parity (that is, the number of
+    1s in the binary representation of the input is even) and 1 if the input has odd
+    parity (that is, the number of 1s in the binary representation of the input is odd).
+
+    @param num The 32-bit integer whose parity is to be computed.
+    @return 0 if even parity, 1 if odd parity.
+*/
+
 #include "major1.h"
 //#include <stdio.h> //for testing purposes
-/**
- * Function to compute the parity of a 32-bit integer.
- * The parity is 0 if the number of 1's in the binary representation is even, 
- * and 1 if the number of 1's is odd.
- *
- * @param num The 32-bit integer whose parity is to be computed.
- * @return 0 if even parity, 1 if odd parity.
- */
+
 int compute_parity(unsigned int num) {
     int parity = 0;
     
