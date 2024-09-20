9fa8267e1a56b0a07d29a3fb59ee05ce93fb57f9
Martin, Corinna <corinnamartin@my.unt.edu>
2024-09-20 03:58:57 +0000

    Update major1.c

--- major1.c
+++ major1.c
@@ -3,7 +3,19 @@
 int main(){
 
     //Display menu
+    int choice;
+
+    printf("Enter the menu option for the operation to perform: \n");
+    printf(" (1)  Count Leading Zeroes\n");
+    printf(" (2)  Endian Swap\n");
+    printf(" (3)  Rotate-right\n");
+    printf(" (4)  Parity\n");
+    printf(" (5)  EXIT\n");
+    printf(" -->  ");
+    
     //Menu selection
+    scanf("%d", &choice);
+    
     //Prompt for integer between 1 and 4294967295, inclusively
     //If “Rotate right” selected, prompt for 2nd int between 1 and 31, inclusively
     //Call chosen operation function
