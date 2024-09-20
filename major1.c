5c5cfa23a2a6f8e85f04398e3ee642bab5e63202
Martin, Corinna <corinnamartin@my.unt.edu>
2024-09-20 03:44:29 +0000

    Update major1.c

---
+++ major1.c
@@ -0,0 +1,12 @@
+#include "major1.h"
+
+int main(){
+
+    //Display menu
+    //Menu selection
+    //Prompt for integer between 1 and 4294967295, inclusively
+    //If “Rotate right” selected, prompt for 2nd int between 1 and 31, inclusively
+    //Call chosen operation function
+
+    return 0;
+}
