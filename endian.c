b0289a1d908addd706f3ca4ae11df967a8835c86
Nguyen, Elisabeth <elisabethnguyen@my.unt.edu>
2024-09-30 04:11:44 +0000

    Update variable types to unsigned int and updated variable name

--- endian.c
+++ endian.c
@@ -13,32 +13,33 @@ void printBin(int num) {
 }
 */
 
-int endianSwap(int init) {
+unsigned int endianSwap(unsigned int num) {
     //original order B3, B2, B1, B0
     //printf() and printBin() for testing
-    //printf("init: ");
-    //printBin(init);
+    //printf("initial: ");
+    //printBin(num);
 
     //B3 and B0 shift left and right respectively to final position
-    int B0 = init << 24;
+    int B0 = num << 24;
     //printf("B0: ");
     //printBin(B0);
 
-    int B3 = init >> 24;
+    int B3 = num >> 24;
     //printf("B3: ");
     //printBin(B3);
 
     //B1 and B2 isolate by shiftng left and right, then shifts to final position
-    int B1 = ((init >> 8) << 24) >> 8;
+    int B1 = ((num >> 8) << 24) >> 8;
     //printf("B1: ");
     //printBin(B1);
 
-    int B2 = ((init << 8) >> 24) << 8;
+    int B2 = ((num << 8) >> 24) << 8;
     //printf("B2: ");
     //printBin(B2);
     
     //final value adds all bytes together
     int final = B0 + B1 + B2 + B3;
+    //printf("final: ")
     //printBin(final);
     return (final);
 }
