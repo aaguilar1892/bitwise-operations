ab38e273ae480915326ceabed03bc25a3b5f9f20
Nelli <NelliSalinas@my.unt.edu>
2024-09-30 14:06:59 -0500

    Updated major1.h

--- major1.h
+++ major1.h
@@ -4,3 +4,4 @@
 int CountLeadingZeroes(unsigned int num);
 int compute_parity(unsigned int num);
 unsigned int endianSwap(unsigned int num);
+unsigned int RotateInt(unsigned int userInt, unsigned int numPos);
