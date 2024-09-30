9386705f1a4fd6671bc8d51c4556a4604e0244bc
Nelli <NelliSalinas@my.unt.edu>
2024-09-30 16:06:38 -0500

    Added comment to rotate.c

--- rotate.c
+++ rotate.c
@@ -3,6 +3,7 @@
 	Author: Nelli Salinas
 	EUID: 	nrs0173
 	Course: 3600.004
+	Date:	9/30/2024
 	Description: This function rotates a given integer x places to the right, where x is chosen by the user.
 	First the integer is shifted to the right x places, leaving x bits as zero on the left side of the number and 
 	the result is stored in shiftOne.To fix that, the integer is then shifted (32-x) places to the left to fill in those zeros from the previous step. 
@@ -11,7 +12,7 @@
 	*/
 
 unsigned int RotateInt(unsigned int userInt, unsigned int numPos){
-	unsigned int shiftOne, shiftTwo, result;
+	unsigned int shiftOne, shiftTwo, result;// Used to store the operations needed to compute the rotation
 	shiftOne = userInt >> numPos;// Shifts integer to the right numPos places
 	shiftTwo = userInt << (32 - numPos);//Shifts the bits on the rightside to the left
 	result = shiftOne | shiftTwo;// Combines both shiftOne and shiftTwo using the bitwise OR operation
